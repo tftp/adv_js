4fbca7ef07c9151370a0beb1d7023ee9c2211c43
OS <goloborodko@inbox.ru>
2020-06-01 21:54:31 +0300

    add ships exercise

--- 06/ships/ships.js
+++ 06/ships/ships.js
@@ -1,45 +1,109 @@
 
 'use strict';
 
-function Ship(name) {
+/**
+ * @typedef {Object} Ship
+ * @property {string} name
+ * @property {string} model
+ * @property {object <{number} x, {number} y>} position
+ */
+
+function Ship(name, model, position) {
     let _isAnchorDroped = false;
+    let _distance = 0;
+
+    if (!name || typeof(name) != 'string' || name.trim().length < 2)
+      throw new Error('Name is wrong');
+
+    if (!model || typeof(model) != 'string' || model.trim().length < 1)
+      throw new Error('Model is wrong');
+
+    if (!position || typeof(position.x) != 'number')
+      throw new Error('Position X is wrong');
+
+    if (!position || typeof(position.y) != 'number')
+      throw new Error('Position Y is wrong');
+
     this.name = name;
-    this.position = { x: 0, y: 0 };
-    this.speed = 0;
+    this.model = model;
+    this.position = position;
+
+    /**
+    * @property {object <{number} x, {number} y>} position
+    * @returns {boolean}
+    */
     this.moveTo = function (position) {
-        if (_isAnchorDroped)
-            throw new Error('You need to rise anchor');
+      if (!position || typeof(position.x) != 'number')
+        throw new Error('Position X is wrong');
 
-        this.position = {
-            x: position.x,
-            y: position.y,
-        }
-    };
+      if (!position || typeof(position.y) != 'number')
+        throw new Error('Position Y is wrong');
 
-    this.isAnchorDroped = function () {
-        console.log('isAnchorDroped', this);
-        return _isAnchorDroped;
+      if (_isAnchorDroped)
+          throw new Error('You need to rise anchor');
+
+      let deviantX = Math.abs(this.position.x - position.x);
+      let deviantY = Math.abs(this.position.y - position.y);
+
+      _distance = _distance + deviantX + deviantY;
+
+      this.position = {
+          x: position.x,
+          y: position.y,
+      };
+
+      return true;
     };
 
     /**
-     * @param {boolean} droped
-     */
-    this.dropAnchor = () => {
-        if (this.speed !== 0)
-            throw new Error('Speed must be 0');
+    * @returns {boolean}
+    */
+    this.isAnchorDroped = function () {
+        return _isAnchorDroped;
+    };
 
+    this.dropAnchor = function() {
         _isAnchorDroped = true;
     };
-}
 
-const ship = new Ship('Best ship');
-ship.moveTo({ x: 10, y: 10 });
-console.log(ship);
+    this.riseAnchor = function() {
+        _isAnchorDroped = false;
+    };
 
-ship.dropAnchor();
+    /**
+    * @returns {number}
+    */
+    this.distance = function(){
+        return _distance;
+    };
 
-ship.moveTo({ x: 20, y: 20 });
-console.log(ship);
+    /**
+    * @property {string} direction
+    * @returns {boolean}
+    */
+    this.move = function(direction) {
+      if (_isAnchorDroped)
+          throw new Error('You need to rise anchor');
 
+      switch (direction) {
+        case 'n':
+            this.position.y += 1;
+            break;
+        case 's':
+            this.position.y -= 1;
+            break;
+        case 'w':
+            this.position.x -= 1;
+            break;
+        case 'e':
+            this.position.x += 1;
+            break;
+        default:
+          throw new Error ('Direction is wrong');
+      };
+      _distance += 1;
+      return true;
+    };
+};
 
-const ship2 = new Ship('Good ship 2');
+const ship = new Ship('ba','A', {x: 11, y: 0})
