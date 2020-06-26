af8747e51be9d5e8dde9659d242e0e2fabc8dfe3
OS <goloborodko@inbox.ru>
2020-06-26 12:09:56 +0300

    correct exercise

--- 10/script.js
+++ 10/script.js
@@ -1,33 +1,52 @@
 API_KEY = "84wP281xkdA9Yr4VF7g2C5RARhdjcoRK";
 
+function getGifs(query){
+  return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`, {
+    cache: 'default'
+  }).then(result => {
+    if(result.ok){
+      //timeRequest = new Date().getTime();
+      return result.json();
+    }
+    throw new Error('Something wrong!')
+  })
+}
+
 const GifsFactory = () => {
   let timeRequest = new Date().getTime();
   return query => {
     if ((new Date().getTime() - timeRequest) < 500){
       return;
     }
-    return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`, {
-      cache: 'default' 
-    }).then(result => {
-      if(result.ok){
-        timeRequest = new Date().getTime();
-        return result.json();
-      }
-      throw new Error('Something wrong!')
-    })
+    // return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`, {
+    //   cache: 'default'
+    // }).then(result => {
+    //   if(result.ok){
+    //     timeRequest = new Date().getTime();
+    //     return result.json();
+    //   }
+    //   throw new Error('Something wrong!')
+    // })
   };
 };
 
-const getGifs = GifsFactory();
+//const getGifs = GifsFactory();
 
 const input = document.querySelector('#gifs-search')
 input.addEventListener('input', (event) =>{ searchGifs(event.target.value) })
-
+let flag;
 const searchGifs = async (query) => {
+  if(flag){
+    setTimeout(() => { flag = false; }, 500);
+
+    return;
+  };
   try {
     const result = await getGifs(query)
-    if (result)
-      console.log(result)
+    if (result){
+      console.log(query, result)
+      flag = true;
+    }
   }catch(err){
     console.error(err)
   };
