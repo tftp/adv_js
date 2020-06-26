54fc62c3c2a3e22be220029853f89fe23118ad74
OS <goloborodko@inbox.ru>
2020-06-26 14:30:57 +0300

    correct 10 exercise

--- 10/script.js
+++ 10/script.js
@@ -1,5 +1,9 @@
 API_KEY = "84wP281xkdA9Yr4VF7g2C5RARhdjcoRK";
 
+const input = document.querySelector('#gifs-search')
+input.addEventListener('input', (event) =>{ searchGifs(event.target.value) })
+let flag;
+
 function getGifs(query){
   return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`, {
     cache: 'default'
@@ -12,29 +16,6 @@ function getGifs(query){
   })
 }
 
-const GifsFactory = () => {
-  let timeRequest = new Date().getTime();
-  return query => {
-    if ((new Date().getTime() - timeRequest) < 500){
-      return;
-    }
-    // return fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API_KEY}`, {
-    //   cache: 'default'
-    // }).then(result => {
-    //   if(result.ok){
-    //     timeRequest = new Date().getTime();
-    //     return result.json();
-    //   }
-    //   throw new Error('Something wrong!')
-    // })
-  };
-};
-
-//const getGifs = GifsFactory();
-
-const input = document.querySelector('#gifs-search')
-input.addEventListener('input', (event) =>{ searchGifs(event.target.value) })
-let flag;
 const searchGifs = async (query) => {
   if(flag){
     setTimeout(() => { flag = false; }, 500);
